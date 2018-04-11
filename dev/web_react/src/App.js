4ef92be683083a8ece7552fd495eea34e43349fa
Marcin Musiałek <m.musialek@hotmail.com>
2018-04-11 23:58:47 +0200

    Add react project.

---
+++ dev/web_react/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './styles/css/styles.css';
+import {Shell} from "./modules/shell/shell";
+
+class App extends Component {
+  render() {
+    return (
+        <Shell></Shell>
+    );
+  }
+}
+
+export default App;
---
+++ dev/web_react/src/modules/shell/shell.js
@@ -0,0 +1,29 @@
+import {Component} from 'react';
+import React from "react";
+
+export class Shell extends Component {
+    render() {
+        return (
+            <div class="vth-shell">
+                <div class="vth-shell__main">
+
+                    <div class="vth-shell__main__left-panel">
+                        left panel
+                        <div class="vth-shell__main__left-panel__details">
+                            details
+                        </div>
+                    </div>
+
+
+                    <div class="vth-shell__main__body">
+                        body
+                    </div>
+
+                </div>
+            </div>
+
+        )
+            ;
+    }
+
+}
