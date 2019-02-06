46f1f12401b09f140df4dd7b7f34d9e2e7fcace7
Marcin Musiałek <m.musialek@hotmail.com>
2019-02-06 22:26:55 +0100

    Update identity server clients.

--- dev/backend/Vetheria.Vtedy.ApiService/IdentityServer/IdentityServerConfig.cs
+++ dev/backend/Vetheria.Vtedy.ApiService/IdentityServer/IdentityServerConfig.cs
@@ -23,9 +23,9 @@ namespace Vetheria.Vtedy.ApiService.IdentityServer
         {
             return new List<ApiResource>
             {
-                new ApiResource("api", "Demo API")
+                new ApiResource("api", "AWR API")
                 {
-                    ApiSecrets = { new Secret("secret".Sha256()) }
+                    ApiSecrets = { new Secret("4124330e-f303-4290-983e-52feae4ffb8b".Sha256()) }
                 }
             };
         }
@@ -34,167 +34,10 @@ namespace Vetheria.Vtedy.ApiService.IdentityServer
         {
             return new List<Client>
             {
-                // native clients
                 new Client
                 {
-                    ClientId = "native.hybrid",
-                    ClientName = "Native Client (Hybrid with PKCE)",
-
-                    RedirectUris = { "https://notused" },
-                    PostLogoutRedirectUris = { "https://notused" },
-
-                    RequireClientSecret = false,
-
-                    AllowedGrantTypes = GrantTypes.Hybrid,
-                    RequirePkce = true,
-                    AllowedScopes = { "openid", "profile", "email", "api" },
-
-                    AllowOfflineAccess = true,
-                    RefreshTokenUsage = TokenUsage.ReUse
-                },
-                new Client
-                {
-                    ClientId = "server.hybrid",
-                    ClientName = "Server-based Client (Hybrid)",
-
-                    RedirectUris = { "https://notused" },
-                    PostLogoutRedirectUris = { "https://notused" },
-
-                    ClientSecrets = { new Secret("secret".Sha256()) },
-
-                    AllowedGrantTypes = GrantTypes.Hybrid,
-                    AllowedScopes = { "openid", "profile", "email", "api" },
-
-                    AllowOfflineAccess = true,
-                    RefreshTokenUsage = TokenUsage.ReUse
-                },
-                new Client
-                {
-                    ClientId = "native.code",
-                    ClientName = "Native Client (Code with PKCE)",
-
-                    RedirectUris = { "https://notused" },
-                    PostLogoutRedirectUris = { "https://notused" },
-
-                    RequireClientSecret = false,
-
-                    AllowedGrantTypes = GrantTypes.Code,
-                    RequirePkce = true,
-                    AllowedScopes = { "openid", "profile", "email", "api" },
-
-                    AllowOfflineAccess = true,
-                    RefreshTokenUsage = TokenUsage.ReUse
-                },
-                new Client
-                {
-                    ClientId = "server.code",
-                    ClientName = "Service Client (Code)",
-
-                    RedirectUris = { "https://notused" },
-                    PostLogoutRedirectUris = { "https://notused" },
-
-                    ClientSecrets = { new Secret("secret".Sha256()) },
-
-                    AllowedGrantTypes = GrantTypes.Code,
-                    AllowedScopes = { "openid", "profile", "email", "api" },
-
-                    AllowOfflineAccess = true,
-                    RefreshTokenUsage = TokenUsage.ReUse
-                },
-
-                // server to server
-                new Client
-                {
-                    ClientId = "client",
-                    ClientSecrets = { new Secret("secret".Sha256()) },
-
-                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
-                    AllowedScopes = { "api" },
-                },
-
-                // SPA per new security guidance
-                new Client
-                {
-                    ClientId = "spa",
-                    ClientName = "SPA (Code + PKCE)",
-
-                    RequireClientSecret = false,
-
-                    RedirectUris = { "https://notused" },
-                    PostLogoutRedirectUris = { "https://notused" },
-
-                    AllowedGrantTypes = GrantTypes.Code,
-                    AllowedScopes = { "openid", "profile", "email", "api" },
-
-                    AllowOfflineAccess = true,
-                    RefreshTokenUsage = TokenUsage.ReUse
-                },
-
-                // implicit (e.g. SPA or OIDC authentication)
-                new Client
-                {
-                    ClientId = "implicit",
-                    ClientName = "Implicit Client",
-                    AllowAccessTokensViaBrowser = true,
-
-                    RedirectUris = { "https://notused" },
-                    PostLogoutRedirectUris = { "https://notused" },
-                    FrontChannelLogoutUri = "http://localhost:5000/signout-idsrv", // for testing identityserver on localhost
-
-                    AllowedGrantTypes = GrantTypes.Implicit,
-                    AllowedScopes = { "openid", "profile", "email", "api" },
-                },
-
-                // implicit using reference tokens (e.g. SPA or OIDC authentication)
-                new Client
-                {
-                    ClientId = "implicit.reference",
-                    ClientName = "Implicit Client using reference tokens",
-                    AllowAccessTokensViaBrowser = true,
-
-                    AccessTokenType = AccessTokenType.Reference,
-
-                    RedirectUris = { "https://notused" },
-                    PostLogoutRedirectUris = { "https://notused" },
-
-                    AllowedGrantTypes = GrantTypes.Implicit,
-                    AllowedScopes = { "openid", "profile", "email", "api" },
-                },
-
-                // implicit using reference tokens (e.g. SPA or OIDC authentication)
-                new Client
-                {
-                    ClientId = "implicit.shortlived",
-                    ClientName = "Implicit Client using short-lived tokens",
-                    AllowAccessTokensViaBrowser = true,
-
-                    AccessTokenLifetime = 70,
-
-                    RedirectUris = { "https://notused" },
-                    PostLogoutRedirectUris = { "https://notused" },
-
-                    AllowedGrantTypes = GrantTypes.Implicit,
-                    AllowedScopes = { "openid", "profile", "email", "api" },
-                },
-                // device flow
-                new Client
-                {
-                    ClientId = "device",
-                    ClientName = "Device Flow Client",
-
-                    AllowedGrantTypes = GrantTypes.DeviceFlow,
-                    RequireClientSecret = false,
-
-                    AllowOfflineAccess = true,
-                    AllowedScopes = { "openid", "profile", "email", "api" }
-                },
-                
-
-                // 
-                new Client
-                {
-                    ClientId = "client1",
-                    ClientSecrets = { new Secret("secret".Sha256()) },
+                    ClientId = "awr_desktop_app",
+                    ClientSecrets = { new Secret("d5e96873-3f37-4855-8e16-0a023ad9d4af".Sha256()) },
 
                     AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
                     AllowedScopes = { "api" },
