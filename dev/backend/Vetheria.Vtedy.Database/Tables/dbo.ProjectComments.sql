9bdf93a8244da1e11d7475230d3360f7529431b7
Marcin Musiałek <m.musialek@hotmail.com>
2019-03-23 23:19:55 +0100

    Add Comment tables. Reorganize database project.

---
+++ dev/backend/Vetheria.Vtedy.Database/Tables/dbo.ProjectComments.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[ProjectComments]
+(
+    [ProjectCommentId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Comment] NVARCHAR(1000) NOT NULL, 
+    [UserAccountId] INT NOT NULL,
+    [ProjectId] INT NOT NULL,
+    [CreatedDateUtc] DateTime2 NOT NULL DEFAULT GETUTCDATE(),
+    [ModifiedDateUtc] DateTime2 NOT NULL DEFAULT GETUTCDATE(),
+    CONSTRAINT FK_ProjectComments_UserAccount_UserAccountId FOREIGN KEY (UserAccountId) REFERENCES UserAccounts(UserAccountId)
+)
---
+++ dev/backend/Vetheria.Vtedy.Database/Tables/dbo.TodoItemComments.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[TodoItemComments]
+(
+    [TodoItemCommentId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Comment] NVARCHAR(1000) NOT NULL, 
+    [UserAccountId] INT NOT NULL,
+    [TodoItemId] UNIQUEIDENTIFIER NOT NULL,
+    [CreatedDateUtc] DateTime2 NOT NULL DEFAULT GETUTCDATE(),
+    [ModifiedDateUtc] DateTime2 NOT NULL DEFAULT GETUTCDATE(),
+    CONSTRAINT FK_TodoItemComments_UserAccount_UserAccountId FOREIGN KEY (UserAccountId) REFERENCES UserAccounts(UserAccountId)
+)
