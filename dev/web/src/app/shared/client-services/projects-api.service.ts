7d2b6ec8e86e82d368d262f433fe1d70c21430cf
Marcin Musiałek <m.musialek@hotmail.com>
2019-06-07 13:07:56 +0200

    Format code.

--- dev/web/src/app/shared/client-services/projects-api.service.ts
+++ dev/web/src/app/shared/client-services/projects-api.service.ts
@@ -5,29 +5,29 @@ import { Observable, of } from 'rxjs';
 import { ApiServiceBase } from './api-service-base';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
-export class ProjectsApiService extends ApiServiceBase{
+export class ProjectsApiService extends ApiServiceBase {
 
-  constructor(private _httpService: HttpClient) {
-    super();
-  }
+    constructor(private _httpService: HttpClient) {
+        super();
+    }
 
-  getProjects(): Observable<ProjectDto[]> {
-    return this._httpService.get<ProjectDto[]>(`${this.baseApiUrl}/Projects`);
-  }
+    getProjects(): Observable<ProjectDto[]> {
+        return this._httpService.get<ProjectDto[]>(`${this.baseApiUrl}/Projects`);
+    }
 
-  getProject(id: number): Observable<ProjectDto> {
-    return this._httpService.get<ProjectDto>(this.baseApiUrl + `/Projects/${id}`);
-  }
+    getProject(id: number): Observable<ProjectDto> {
+        return this._httpService.get<ProjectDto>(this.baseApiUrl + `/Projects/${id}`);
+    }
 
-  update(project: ProjectDto): Observable<ProjectDto> {
-    const request = project;
-    return this._httpService.put<ProjectDto>(`${this.baseApiUrl}/Projects/${project.projectId}`, request);
-  }
+    update(project: ProjectDto): Observable<ProjectDto> {
+        const request = project;
+        return this._httpService.put<ProjectDto>(`${this.baseApiUrl}/Projects/${project.projectId}`, request);
+    }
 
-  add(project: ProjectDto): Observable<ProjectDto> {
-    const request = project;
-    return this._httpService.post<ProjectDto>(`${this.baseApiUrl}/Projects`, request);
-  }
+    add(project: ProjectDto): Observable<ProjectDto> {
+        const request = project;
+        return this._httpService.post<ProjectDto>(`${this.baseApiUrl}/Projects`, request);
+    }
 }
